a3e4a71e7b7c0c52afa6192dc527b651d30abfd1
Stephen Condon <stephen.condon2@gmail.com>
2023-08-20 13:33:33 +0100

    Fix incorrect git rebase

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -2,11 +2,7 @@
 
 {
   xdg.configFile.hypr = {
-<<<<<<< HEAD
     source = ./config;
     recursive = true;
-=======
-    source = ./hyprland.conf;
->>>>>>> 2e63043 (Added starting hyprland conf)
   };
 }
