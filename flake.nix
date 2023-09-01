f41d50754379e0a07cbe6654ce112912b7a32a54
Stephen Condon <stephen.condon2@gmail.com>
2023-09-01 20:30:32 +0100

    Revert "Tried to fix that error again"
    This reverts commit 16b9310d631da0ea14cf6757430a282558035756.

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
               home-manager = {
                 useUserPackages = true;
                 useGlobalPkgs = true;
-                users.stephen = import ./home/default.nix { machine = "desktop"; };
+                users.stephen = ./home/default.nix { machine = "desktop"; };
               };
             }
           ];
