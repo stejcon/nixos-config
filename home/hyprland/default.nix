33fe05be96400b82a1ede5e93fa3a5ebaa07295a
Stephen Condon <stephen.condon2@gmail.com>
2023-09-01 21:04:15 +0100

    Change hyprland background based on hostname

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,8 +1,17 @@
-{ config, pkgs, lib, ... }:
+{ osConfig, ... }:
 
 {
   xdg.configFile.hypr = {
     source = ./config;
     recursive = true;
+  };
+
+  xdg.configFile.hypr."background.webp" = if osConfig.networking.hostName == "thor" then {
+    source = ./loki-background.webp;
+    recursive = false;
+  }
+  else { # If not on a laptop default to 1920x1080 image
+    source = ./thor-background.webp;
+    recursive = false;
   };
 }
