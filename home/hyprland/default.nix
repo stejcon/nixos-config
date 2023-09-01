189e36790c16e375001694eecaee3016f85041bb
Stephen Condon <stephen.condon2@gmail.com>
2023-09-01 21:51:45 +0100

    Fix config
    I was an idiot sandwich. The only error was thor instead of loki.

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,11 +1,11 @@
-{config, ...}: {
+{osConfig, ...}: {
   xdg.configFile.hypr = {
     source = ./config;
     recursive = true;
   };
 
   xdg.configFile."hypr/background.webp".source =
-    if config.networking.hostName == "loki"
+    if osConfig.networking.hostName == "loki"
     then ./loki-background.webp
     else ./thor-background.webp;
 }
