2d2aabf63def4b6d19537f381298c584c904e779
Stephen Condon <stephen.condon2@gmail.com>
2023-09-01 21:46:02 +0100

    Use config like its supposed to be instead of the way an idiot would do it

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,11 +1,11 @@
-{osConfig, ...}: {
+{config, ...}: {
   xdg.configFile.hypr = {
     source = ./config;
     recursive = true;
   };
 
   xdg.configFile."hypr/background.webp".source =
-    if osConfig.networking.hostName == "thor"
+    if config.networking.hostName == "loki"
     then ./loki-background.webp
     else ./thor-background.webp;
 }
