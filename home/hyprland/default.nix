ce21c66607024c4259eaf944bb457ecc843c82b4
Stephen Condon <stephen.condon2@gmail.com>
2023-10-09 01:49:17 +0100

    Update hyprland

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -32,9 +32,8 @@
         gaps_in = 10;
         gaps_out = 20;
         border_size = 1;
-        col = {
-          active_border = "rgba(00ffffee)";
-          inactive_border = "rgba(595959aa)";
+        "col.active_border" = "rgba(00ffffee)";
+        "col.inactive_border" = "rgba(595959aa)";
         };
         layout = "dwindle";
         cursor_inactive_timeout = 5;
@@ -50,7 +49,7 @@
         drop_shadow = false;
         shadow_range = 4;
         shadow_render_power = 3;
-        col = {shadow = "rgba(1a1a1aee)";};
+        "col.shadow" = "rgba(1a1a1aee)";
         inactive_opacity = 0.8;
       };
 
