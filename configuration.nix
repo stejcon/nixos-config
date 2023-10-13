8de44573335e9247f98586227cfedbd26a606c31
Stephen Condon <stephen.condon2@gmail.com>
2023-10-13 13:44:24 +0100

    Make the nixpkgs registry entry the same nixpkgs as the system is built with

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{pkgs, inputs, ...}: {
   nixpkgs.config.allowUnfree = true;
 
   # Use the grub EFI boot loader.
@@ -118,5 +118,6 @@
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
+    registry.nixpkgs.flake = inputs.nixpkgs;
   };
 }
