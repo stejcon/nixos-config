65fab18bd2eba526b78e574cc2a9a4bdf2ccbd76
Stephen Condon <stephen.condon2@gmail.com>
2023-10-14 15:14:12 +0100

    Move nixpkgs config to check if it works

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.config.allowUnfree = true;
+
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
