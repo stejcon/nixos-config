97f2c1b2d672958307c23098f232be2de5d723a5
Stephen Condon <stephen.condon2@gmail.com>
2023-10-14 15:48:42 +0100

    Add extraModules field to mkHome

--- lib/mkHome.nix
+++ lib/mkHome.nix
@@ -2,8 +2,9 @@
   home-manager,
   nixpkgs,
   system ? "x86_64-linux",
+  extraModules ? [],
 }:
 home-manager.lib.homeManagerConfiguration {
   pkgs = import nixpkgs {inherit system;};
-  modules = [../home.nix];
+  modules = [../home.nix] ++ extraModules;
 }
