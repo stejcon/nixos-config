1800e07fd85613c0b0e6d53c66df5aa1df7aec58
stejcon <66426838+stejcon@users.noreply.github.com>
2023-12-31 19:15:06 +0000

    Swaylock swayidle (#12)
    * Add pam swaylock settings
    
    * Add config settings
    
    * fmt
    
    * Add monitor off for hyprland, low timeout to test
    
    * Works, increase timeout to 30 minutes
    
    * Add TODO

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,4 +1,58 @@
-{osConfig, ...}: {
+{
+  osConfig,
+  pkgs,
+  config,
+  ...
+}: {
+  programs = {
+    swaylock = {
+      enable = true;
+      package = pkgs.swaylock-effects;
+      settings = {
+        daemonize = true;
+        screenshots = true;
+        clock = true;
+        indicator = true;
+        indicator-radius = 120;
+        indicator-thickness = 10;
+        effect-blur = "7x10";
+        effect-vignette = "0.5:0.5";
+        text-color = "eeeeee";
+        ring-color = "005678";
+        key-hl-color = "ff2a6d";
+        line-color = "00000000";
+        inside-color = "00000088";
+        separator-color = "00000000";
+        fade-in = 1.0;
+      };
+    };
+  };
+
+  services = {
+    # TODO: Add a timeout to suspend the computer
+    swayidle = {
+      enable = true;
+      timeouts = [
+        {
+          timeout = 600;
+          command = "${config.programs.swaylock.package}/bin/swaylock";
+        }
+        {
+          timeout = 1800;
+          command = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms off";
+          resumeCommand = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms on";
+        }
+      ];
+      events = [
+        {
+          event = "before-sleep";
+          command = "${config.programs.swaylock.package}/bin/swaylock";
+        }
+      ];
+      systemdTarget = "hyprland-session.target";
+    };
+  };
+
   wayland.windowManager = {
     hyprland.enable = true;
 
