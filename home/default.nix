ab55f9102eb240d987da15c36a8eece9924dc5d0
Stephen Condon <stephen.condon2@gmail.com>
2024-01-01 00:09:56 +0000

    add lf

--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,5 @@
 {...}: {
-  imports = [./neovim ./kitty ./tmux ./git ./hyprland ./bluetooth ./zsh ./waybar];
+  imports = [./neovim ./kitty ./tmux ./git ./hyprland ./bluetooth ./zsh ./waybar ./lf];
 
   home.username = "stephen";
   home.homeDirectory = "/home/stephen";
