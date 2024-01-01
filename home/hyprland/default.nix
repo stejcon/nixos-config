2f819dab5b972746ef1fcd2c682a8e52314fdfef
Stephen Condon <stephen.condon2@gmail.com>
2024-01-01 01:24:22 +0000

    (fix): broken bindings

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -209,8 +209,8 @@ in {
           "$mainMod SHIFT, q, exit,"
           "$mainMod      , e, exec, ${pkgs.kitty}/bin/kitty -e ${pkgs.lf}/bin/lf"
           "$mainMod      , v, togglefloating,"
-          "$mainMod      , p, exec, ${pkgs.wofi} --show drun"
-          "$mainMod SHIFT, s, exec, ${pkgs.grimblast} --notify copysave area"
+          "$mainMod      , p, exec, ${pkgs.wofi}/bin/wofi --show drun"
+          "$mainMod SHIFT, s, exec, ${pkgs.grimblast}/bin/grimblast --notify copysave area"
           "$mainMod      , f, fullscreen"
 
           "$mainMod, left, movefocus, l"
