f0a6c8e97cb5dc5702bbbeb0e073d26cae7fc8a4
Stephen Condon <stephen.condon2@gmail.com>
2024-01-03 22:55:44 +0000

    Add test script

--- home/spotifyd/default.nix
+++ home/spotifyd/default.nix
@@ -22,6 +22,55 @@
     # Clean up temporary files
     rm "$tmp_image" "$tmp_png"
   '';
+  test-update = pkgs.writeShellScriptBin "update" ''
+state="$PLAYER_EVENT"
+
+datadir="$HOME/.data/spotify"
+data_log="$datadir/event-log"
+data_status="$datadir/status"
+data_metadata="$datadir/metadata"
+
+function has_metadata {
+  if [[ ! -f "$data_metadata" ]]; then
+    return 1
+  fi
+
+  player_metadata="$(cat $data_metadata)"
+  if [ ["$player_metadata" == ""] ]; then
+    return 1
+  fi
+}
+
+function update_metadata {
+  contents="$(playerctl metadata)"
+  echo "$contents" > $data_metadata
+}
+
+case "$state" in
+  "change")
+    echo "player status: change" >> $data_log
+    update_metadata
+  ;;
+  "play")
+    echo "player status: play" >> $data_log
+    echo "playing" > $data_status
+    has_metadata || update_metadata
+  ;;
+  "pause")
+    echo "player status: pause" >> $data_log
+    echo "paused" > $data_status
+    has_metadata || update_metadata
+  ;;
+  "stop")
+    echo "player status: stop" >> ~/data/spotify/status
+    echo "stopped" > $data_status
+    echo "" > $data_metadata
+  ;;
+  *)
+    echo "event $state" >> $data_log
+  ;;
+esac
+  '';
 in {
   services.spotifyd = {
     enable = true;
@@ -42,7 +91,7 @@ in {
         volume-normalisation = true;
         normalisation-pregain = -10;
         initial_volume = "100";
-        on_song_change_hook = "${spotify-update}/bin/update";
+        on_song_change_hook = "${test-update}/bin/update";
       };
     };
   };
