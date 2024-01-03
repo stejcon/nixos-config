eeb16efd5c80142d8f49e59328fde95b23b24d80
Stephen Condon <stephen.condon2@gmail.com>
2024-01-03 14:59:35 +0000

    fmt

--- home/spotifyd/default.nix
+++ home/spotifyd/default.nix
@@ -1,25 +1,24 @@
-{pkgs, ...}:
-{
-      services.spotifyd = {
-        enable = true;
-        package = pkgs.spotifyd.override {
-          withMpris = true;
-          withPulseAudio = true;
-        };
-        settings = {
-          global = {
-            backend = "alsa";
-            device = "default";
-            mixer = "PCM";
-            volume-controller = "alsa";
-            device_name = "Speaker";
-            device_type = "speaker";
-            bitrate = 320;
-            no_audio_cache = true;
-            volume-normalisation = true;
-            normalisation-pregain = -10;
-            initial_volume = "100";
-          };
-        };
+{pkgs, ...}: {
+  services.spotifyd = {
+    enable = true;
+    package = pkgs.spotifyd.override {
+      withMpris = true;
+      withPulseAudio = true;
+    };
+    settings = {
+      global = {
+        backend = "alsa";
+        device = "default";
+        mixer = "PCM";
+        volume-controller = "alsa";
+        device_name = "Speaker";
+        device_type = "speaker";
+        bitrate = 320;
+        no_audio_cache = true;
+        volume-normalisation = true;
+        normalisation-pregain = -10;
+        initial_volume = "100";
       };
+    };
+  };
 }
