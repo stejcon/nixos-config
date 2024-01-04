d716d024aad7776dfe434feb7b412315c6704e3b
stejcon <66426838+stejcon@users.noreply.github.com>
2024-01-04 15:18:48 +0000

    Zoxide (#20)
    * add default zoxide with bash/zsh integration
    
    * add zoxide to home config

--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,5 @@
 {...}: {
-  imports = [./neovim ./kitty ./tmux ./git ./hyprland ./bluetooth ./zsh ./waybar ./lf ./zellij];
+  imports = [./neovim ./kitty ./tmux ./git ./hyprland ./bluetooth ./zsh ./waybar ./lf ./zellij ./zoxide];
 
   home.username = "stephen";
   home.homeDirectory = "/home/stephen";
