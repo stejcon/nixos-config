0660d42b26b15c26b5966a6b2d4264841635a3b9
Stephen Condon <stephen.condon2@gmail.com>
2024-01-04 15:58:37 +0000

    add nix_shell extensions instead of direnv

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -5,10 +5,14 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
     settings = {
-      format = "$directory$git_branch$git_metrics$fill$direnv";
+      format = "$directory$git_branch$git_metrics$fill$nix_shell";
 
       fill = {
         symbol = "";
+      };
+
+      nix_shell = {
+        format = "[$symbol$state( \($name\))]($style)";
       };
     };
   };
