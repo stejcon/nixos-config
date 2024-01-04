3925440813d98112b8692bb4fbda6fd298c1a610
Stephen Condon <stephen.condon2@gmail.com>
2024-01-04 00:35:04 +0000

    cant have compact mode yet cause i need the keybind hints, can reenable later

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -6,7 +6,7 @@
     enableZshIntegration = true;
     settings = {
       pane_frames = false;
-      default_layout = "compact";
+      #default_layout = "compact";
       mouse_mode = false;
       copy_command = "wl-copy";
       theme = "catppuccin-mocha";
