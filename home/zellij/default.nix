b95c71345ac4be031290c910ffb8c79f1fa9d864
Stephen Condon <stephen.condon2@gmail.com>
2024-01-04 00:32:01 +0000

    add zellij

---
+++ home/zellij/default.nix
@@ -0,0 +1,15 @@
+{...}:
+{
+  programs.zellij = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    settings = {
+      pane_frames = false;
+      default_layout = "compact";
+      mouse_mode = false;
+      copy_command = "wl-copy";
+      theme = "catppuccin-mocha";
+    };
+  };
+}
