c49b1f335c62453d71dfac835488b42c56cb3908
Stephen Condon <stephen.condon2@gmail.com>
2024-01-09 18:02:55 +0000

    make kitty more nix-like

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -1,18 +1,13 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{...}: {
   programs.kitty = {
     enable = true;
     font.name = "JetBrainsMono Nerd Font Mono";
     font.size = 14;
     theme = "Catppuccin-Mocha";
-    extraConfig = ''
-      active_tab_font_style bold
-      inactive_tab_font_style italic
-      confirm_os_window_close 0
-    '';
+    settings = {
+      active_tab_font_style = "bold";
+      inactive_tab_font_style = "italic";
+      confirm_os_window_close = 0;
+    };
   };
 }
