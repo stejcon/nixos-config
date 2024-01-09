00c006bbb03a30fbaefb64fcc13ef357fd3e3c6d
Stephen Condon <stephen.condon2@gmail.com>
2024-01-09 02:24:48 +0000

    temporarily disable neovim config, replace with nixvim

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -16,103 +16,104 @@ with lib; let
     };
   };
 in {
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    defaultEditor = true;
+  imports = [./nixvim.nix]
+  # programs.neovim = {
+  #   enable = true;
+  #   viAlias = true;
+  #   vimAlias = true;
+  #   vimdiffAlias = true;
+  #   defaultEditor = true;
 
-    plugins = with pkgs.vimPlugins; [
-      plenary-nvim # Config
+  #   plugins = with pkgs.vimPlugins; [
+  #     plenary-nvim # Config
 
-      # Colorschemes
-      gruvbox-community
-      everforest
-      oxocarbon-nvim
-      nightfox-nvim
-      onedark-nvim
-      kanagawa-nvim
-      oh-lucy-nvim
+  #     # Colorschemes
+  #     gruvbox-community
+  #     everforest
+  #     oxocarbon-nvim
+  #     nightfox-nvim
+  #     onedark-nvim
+  #     kanagawa-nvim
+  #     oh-lucy-nvim
 
-      # Telescope/Treesitter
-      telescope-nvim
-      dressing-nvim # Config
-      telescope-fzf-native-nvim
-      nvim-treesitter.withAllGrammars
+  #     # Telescope/Treesitter
+  #     telescope-nvim
+  #     dressing-nvim # Config
+  #     telescope-fzf-native-nvim
+  #     nvim-treesitter.withAllGrammars
 
-      # LSP
-      nvim-lspconfig
-      null-ls-nvim
-      fidget-nvim
-      nvim-lightbulb
-      nvim-lsp-ts-utils
+  #     # LSP
+  #     nvim-lspconfig
+  #     null-ls-nvim
+  #     fidget-nvim
+  #     nvim-lightbulb
+  #     nvim-lsp-ts-utils
 
-      # Completion
-      luasnip
-      nvim-cmp
-      cmp-nvim-lsp
-      cmp-buffer
-      cmp-path
-      cmp-cmdline
-      cmp-nvim-lsp-signature-help
-      cmp_luasnip
-      lspkind-nvim
+  #     # Completion
+  #     luasnip
+  #     nvim-cmp
+  #     cmp-nvim-lsp
+  #     cmp-buffer
+  #     cmp-path
+  #     cmp-cmdline
+  #     cmp-nvim-lsp-signature-help
+  #     cmp_luasnip
+  #     lspkind-nvim
 
-      # DAP
-      nvim-dap
-      telescope-dap-nvim
-      nvim-dap-ui
-      nvim-dap-virtual-text
+  #     # DAP
+  #     nvim-dap
+  #     telescope-dap-nvim
+  #     nvim-dap-ui
+  #     nvim-dap-virtual-text
 
-      # Keys
-      legendary-nvim
-      which-key-nvim
+  #     # Keys
+  #     legendary-nvim
+  #     which-key-nvim
 
-      # Misc
-      vimtex
-      oil-nvim
-      neorg
-      lualine-nvim
-      nvim-web-devicons
-      gitsigns-nvim
-      vim-illuminate
-      nvim-notify
-      no-neck-pain-nvim
-    ];
+  #     # Misc
+  #     vimtex
+  #     oil-nvim
+  #     neorg
+  #     lualine-nvim
+  #     nvim-web-devicons
+  #     gitsigns-nvim
+  #     vim-illuminate
+  #     nvim-notify
+  #     no-neck-pain-nvim
+  #   ];
 
-    extraPackages = with pkgs; [
-      tree-sitter
-      nodejs
-      ripgrep
-      fd
+  #   extraPackages = with pkgs; [
+  #     tree-sitter
+  #     nodejs
+  #     ripgrep
+  #     fd
 
-      # Language Servers
-      nil
-      nixpkgs-fmt
-      statix # Nix
-      clang-tools_16 # Clangd and other clang stuff
-      pyright
-      black
-      python-debug # Python
-      rust-analyzer
-      rustfmt # Rust
-      lua-language-server # Lua
-      java-language-server
-      kotlin-language-server # Java/Kotlin
-      nodePackages.typescript-language-server
-      nodePackages.vscode-langservers-extracted # Webdev
-      haskell-language-server # Haskell
-    ];
+  #     # Language Servers
+  #     nil
+  #     nixpkgs-fmt
+  #     statix # Nix
+  #     clang-tools_16 # Clangd and other clang stuff
+  #     pyright
+  #     black
+  #     python-debug # Python
+  #     rust-analyzer
+  #     rustfmt # Rust
+  #     lua-language-server # Lua
+  #     java-language-server
+  #     kotlin-language-server # Java/Kotlin
+  #     nodePackages.typescript-language-server
+  #     nodePackages.vscode-langservers-extracted # Webdev
+  #     haskell-language-server # Haskell
+  #   ];
 
-    extraConfig = ''
-      let g:python_debug_home = "${python-debug}"
-      :luafile ~/.config/nvim/lua/init.lua
-    '';
-  };
+  #   extraConfig = ''
+  #     let g:python_debug_home = "${python-debug}"
+  #     :luafile ~/.config/nvim/lua/init.lua
+  #   '';
+  # };
 
-  xdg.configFile.nvim = {
-    source = ./config;
-    recursive = true;
-  };
+  # xdg.configFile.nvim = {
+  #   source = ./config;
+  #   recursive = true;
+  # };
 }
