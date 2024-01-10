a17d2c10073fd32617091aef84735001d847e9cc
Stephen Condon <stephen.condon2@gmail.com>
2024-01-10 00:04:00 +0000

    extraSettings -> extraOptions

--- home/neovim/nixvim.nix
+++ home/neovim/nixvim.nix
@@ -108,7 +108,7 @@
           nil_ls = {
             enable = true;
             settings.formatting.command = ["${pkgs.alejandra}/bin/alejandra"];
-            extraSettings = {autoArchive = true;};
+            extraOptions.settings = {autoArchive = true;};
           };
           pyright.enable = true;
           typst-lsp.enable = true;
