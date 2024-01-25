e56d6dfa02bc0d3324b973226f5a44253e8c4495
Stephen Condon <stephen.condon2@gmail.com>
2024-01-25 12:00:26 +0000

    add fwupd

--- hosts/loki/hardware-configuration.nix
+++ hosts/loki/hardware-configuration.nix
@@ -41,4 +41,5 @@
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.bluetooth.enable = true;
+  services.fwupd.enable = true;
 }
