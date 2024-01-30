e177b49e2893ba82c09c45941269955d7e4862b1
Stephen Condon <stephen.condon2@gmail.com>
2024-01-30 17:08:54 +0000

    change CR mapping in nvim-cmp

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -149,7 +149,7 @@
             modes = ["i" "s"];
             action = "cmp.mapping.select_prev_item()";
           };
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<CR>" = "cmp.mapping.confirm({ select = false })";
         };
       };
       lsp = {
