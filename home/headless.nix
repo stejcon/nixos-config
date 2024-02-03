ddb9f0d69cf73d2417dd831f05b5d7f1c42b9afd
Stephen Condon <stephen.condon2@gmail.com>
2024-02-03 15:09:55 +0000

    added headless home

---
+++ home/headless.nix
@@ -0,0 +1,18 @@
+{...}: {
+  imports = [
+    ./neovim
+    ./git
+    ./zsh
+    ./lf
+    ./zellij
+    ./zoxide
+    ./starship
+    ./direnv
+  ]
+
+  home = {
+    username = "stephen";
+    homeDirectory = "/home/stephen";
+    stateVersion = "23.11";
+  };
+}
