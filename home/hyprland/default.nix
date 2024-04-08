19e557297f368f6d547dec12cf926cdccd41e8a0
Stephen Condon <stephen.condon2@gmail.com>
2024-04-08 13:28:09 +0100

    dont need logs

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -147,10 +147,6 @@ in {
         sensitivity = 0;
       };
 
-      debug = {
-        disable_logs = false;
-      };
-
       misc = {
         disable_splash_rendering = true;
         disable_hyprland_logo = true;
