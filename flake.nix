65a49638de4c5b07d3630dee51fcd72d0bb529d9
Stephen Condon <stephen.condon2@gmail.com>
2024-05-07 19:22:55 +0100

    change hyprland url

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
     hyprsplit = {
       url = "github:shezdy/hyprsplit";
