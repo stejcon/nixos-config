9f34cd29ec0d48ff0cf4ba0ddc18f4f1328f1ad7
Stephen Condon <stephen.condon2@gmail.com>
2024-06-24 13:04:24 +0100

    move autocomplete to its own file

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -6,6 +6,7 @@
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
     ./settings.nix
+    ./autocomplete.nix
   ];
 
   programs.nixvim = {
@@ -38,9 +39,6 @@
         };
       };
       fidget = {
-        enable = true;
-      };
-      luasnip = {
         enable = true;
       };
       treesitter = {
@@ -117,29 +115,6 @@
             options = {
               desc = "Telescope: Buffers";
             };
-          };
-        };
-      };
-      cmp = {
-        enable = true;
-        autoEnableSources = true;
-        settings = {
-          snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-          preselect = "cmp.PreselectMode.None";
-          sources = [
-            {name = "nvim_lsp";}
-            {name = "path";}
-            {name = "buffer";}
-            {name = "luasnip";}
-          ];
-          mapping = {
-            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<C-Space>" = "cmp.mapping.complete()";
-            "<C-e>" = "cmp.mapping.close()";
-            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-            "<CR>" = "cmp.mapping.confirm({ select = false })";
           };
         };
       };
