0b5a801dc8876ebe54ea70fcc9544a30d93c27d9
Stephen Condon <stephen.condon2@gmail.com>
2024-06-24 16:42:39 +0100

    moved last pieces to their own files

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -4,62 +4,18 @@
     ./settings.nix
     ./autocomplete.nix
     ./autocommands.nix
+    ./colorscheme.nix
+    ./keymaps.nix
     ./plugins
   ];
 
   programs.nixvim = {
     enable = true;
-    colorschemes = {
-      catppuccin = {
-        enable = true;
-        settings = {
-          flavour = "mocha";
-          show_end_of_buffer = true;
-        };
-      };
-    };
-    keymaps = [
-      {
-        key = "<Space>";
-        action = "<NOP>";
-        options.silent = true;
-      }
-      {
-        key = "<Esc>";
-        mode = ["n"];
-        action = ":noh<CR><ESC>";
-        options.silent = true;
-      }
-      {
-        key = "<Space>h";
-        mode = ["n"];
-        action = "<C-w>h";
-        options.desc = "Window: Focus left";
-      }
-      {
-        key = "<Space>j";
-        mode = ["n"];
-        action = "<C-w>j";
-        options.desc = "Window: Focus down";
-      }
-      {
-        key = "<Space>k";
-        mode = ["n"];
-        action = "<C-w>k";
-        options.desc = "Window: Focus up";
-      }
-      {
-        key = "<Space>l";
-        mode = ["n"];
-        action = "<C-w>l";
-        options.desc = "Window: Focus right";
-      }
-      {
-        key = "<leader>w";
-        mode = ["n"];
-        action = ":silent! TypstWatch<CR>";
-        options.desc = "Typst Watch";
-      }
-    ];
+    defaultEditor = true;
+
+    viAlias = true;
+    vimAlias = true;
+
+    luaLoader.enable = true;
   };
 }
