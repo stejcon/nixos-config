64c89cc74337b489cf557fa3d65806e1fd71e441
Stephen Condon <stephen.condon2@gmail.com>
2024-06-30 22:52:29 +0100

    make hyprland exec ags

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -262,7 +262,10 @@ in {
       exec-once = [
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "${pkgs.bash}/bin/bash ${startScript}/bin/start"
-        "waybar"
+      ];
+
+      exec = [
+        "ags"
       ];
     };
   };
