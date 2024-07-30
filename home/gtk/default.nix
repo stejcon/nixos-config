8ff1561138c5eac2e9171d5e92f5ebd48ad74e88
Stephen Condon <stephen.condon2@gmail.com>
2024-07-30 11:58:19 +0100

    change gtk theme

--- home/gtk/default.nix
+++ home/gtk/default.nix
@@ -1,13 +1,17 @@
 {pkgs, ...}: {
   gtk = {
     enable = true;
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+    };
     theme = {
-      name = "rose-pine-gtk";
-      package = pkgs.rose-pine-gtk-theme;
+      package = pkgs.adw-gkt3;
+      name = "adw-gtk3";
     };
     iconTheme = {
-      name = "rose-pine-icons";
-      package = pkgs.rose-pine-gtk-theme;
+      package = pkgs.gruvboxPlus;
+      name = "GruvboxPlus";
     };
   };
 }
