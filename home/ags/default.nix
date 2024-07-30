a5e69ac14f73b0872062771a6a547c288b9d13e1
Stephen Condon <stephen.condon2@gmail.com>
2024-07-30 14:04:33 +0100

    use Aylur config

--- home/ags/default.nix
+++ home/ags/default.nix
@@ -3,19 +3,33 @@
   pkgs,
   ...
 }: {
-  imports = [inputs.ags.homeManagerModules.default];
+  imports = [
+    inputs.ags.homeManagerModules.default
+  ];
+
+  home.packages = with pkgs; [
+    bun
+    dart-sass
+    fd
+    brightnessctl
+    swww
+    inputs.matugen.packages.${system}.default
+    slurp
+    wf-recorder
+    wl-clipboard
+    wayshot
+    swappy
+    hyprpicker
+    pavucontrol
+    networkmanager
+    gtk3
+  ];
 
   programs.ags = {
     enable = true;
-
     configDir = ./config;
-
     extraPackages = with pkgs; [
-      gtksourceview
-      webkitgtk
       accountsservice
     ];
   };
-
-  home.packages = with pkgs; [bun];
 }
